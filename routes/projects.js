91f59cd719a21e462549bb5f26a8caa0267fbc0c
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-09 13:50:06 -0600

    Set up issues routes.. still working out some kinks

--- routes/projects.js
+++ routes/projects.js
@@ -20,6 +20,22 @@ module.exports = function(app, isLoggedIn) {
         });
     });
 
+    app.get("/projects/:id", isLoggedIn, function(req, res) {
+        var id = req.params.id;
+
+        db.Project.findOne({_id: id}).populate("issues").then(function(dbProject) {
+            console.log(dbProject);
+
+            res.render("issues", data = {
+                user: req.user,
+                project: dbProject
+            })
+
+        }).catch(function(err) {
+            if (err) {console.log(err)};
+        })
+    });
+
     app.post("/addproject", function(req, res) {
         db.Project.create(req.body).then(function(dbProject) {
             return db.User.findOneAndUpdate(
@@ -29,6 +45,23 @@ module.exports = function(app, isLoggedIn) {
             );
         }).then(function(dbUser) {
             res.redirect("/projects");
+        }).catch(function(err) {
+            console.log(err);
+        });
+    });
+
+    app.post("/addissue/:id", function(req, res) {
+        var id = req.params.id
+
+        db.Issue.create(req.body).then(function(dbIssue) {
+            issue = dbIssue;
+            return db.Project.findOneAndUpdate(
+                {_id: id},
+                { $push: {issues: dbIssue.id}},
+                {new: true}
+            );
+        }).then(function(dbProject) {
+            res.redirect("/projects/" + id);
         }).catch(function(err) {
             console.log(err);
         });

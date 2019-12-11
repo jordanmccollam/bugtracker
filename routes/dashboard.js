0b2ae0e0577cf0b0451d1b7383c60a98472a3cb9
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-11 17:35:02 -0600

    Tried to mess with progress or overview section. Unsuccessfully

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,9 +4,6 @@ module.exports = function(app, isLoggedIn) {
 
     app.get("/dashboard", isLoggedIn, function(req, res) {
         db.User.findOne({_id: req.user.id}).populate("projects").then(function(dbUser) {
-            // TODO in projects.handlebars I'm using an additional if statement.
-            // It seems handlebars thinks that var project is the entire user or something
-            // this should be fixed for optimization 
 
             var projects = dbUser.projects;
 

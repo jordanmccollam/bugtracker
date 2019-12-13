a3544deffb2ff417407d97917e9775525fd68051
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-12 20:44:41 -0600

    Fixed the new cascading delete for projects so it deletes itself from parent too

--- routes/deleteRoutes.js
+++ routes/deleteRoutes.js
@@ -5,10 +5,12 @@ module.exports = function (app) {
     // DELETE ---
 
     // Delete project
-    app.delete("/delete/:id", function(req, res) {
+    app.delete("/delete/:id", function (req, res) {
         var id = req.params.id;
 
-        db.Project.findOne({_id: id}).populate("issues").then(function(dbProject) {
+        db.Project.findOne({
+            _id: id
+        }).populate("issues").then(function (dbProject) {
 
             // Loop through every issue and delete its comments
             for (var i = 0; i < dbProject.issues.length; i++) {
@@ -16,8 +18,10 @@ module.exports = function (app) {
                     _id: {
                         $in: dbProject.issues[i].comments
                     }
-                }, function(err) {
-                    if (err) {console.log(err)};
+                }, function (err) {
+                    if (err) {
+                        console.log(err)
+                    };
                 })
             }
 
@@ -26,38 +30,66 @@ module.exports = function (app) {
                 _id: {
                     $in: dbProject.issues
                 }
-            }, function(err) {
-                if (err) {console.log(err)};
+            }, function (err) {
+                if (err) {
+                    console.log(err)
+                };
 
                 // Delete project and redirect
                 dbProject.remove();
-                res.redirect("/dashboard");
+
+                // Delete from parent
+                db.User.findOneAndUpdate({
+                    _id: req.user.id
+                }, {
+                    $pull: {
+                        projects: id
+                    }
+                }).then(function (dbUser) {
+                    console.log("REMOVED " + id);
+                }).catch(function (err) {
+                    if (err) {
+                        console.log(err)
+                    }
+                });
+
             });
 
-        }).catch(function(err) {
-            if (err) {console.log(err)};
+        }).catch(function (err) {
+            if (err) {
+                console.log(err)
+            };
         });
 
     });
 
     // Delete Issue
-    app.delete("/delete/:projectID/:issueID", function(req, res) {
+    app.delete("/delete/:projectID/:issueID", function (req, res) {
         var projectID = req.params.projectID;
         var issueID = req.params.issueID;
 
-        db.Issue.deleteOne({_id: issueID}).then(function(dbIssue) {
-            
-            db.Project.findOneAndUpdate(
-                {_id: projectID},
-                { $pull: {issues: issueID}}
-            ).then(function(dbUser) {
+        db.Issue.deleteOne({
+            _id: issueID
+        }).then(function (dbIssue) {
+
+            db.Project.findOneAndUpdate({
+                _id: projectID
+            }, {
+                $pull: {
+                    issues: issueID
+                }
+            }).then(function (dbUser) {
                 console.log("REMOVED " + id);
-            }).catch(function(err) {
-                if (err) {console.log(err)}
+            }).catch(function (err) {
+                if (err) {
+                    console.log(err)
+                }
             });
 
-        }).catch(function(err) {
-            if (err) {console.log(err)};
+        }).catch(function (err) {
+            if (err) {
+                console.log(err)
+            };
         });
     });
 
